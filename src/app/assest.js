4120f5ac6a100e14ab1f572914f29a679683aed6
Prashant <prashantdubey0098@gmail.com>
2024-02-05 00:31:28 +0530

    updated logo

---
+++ src/app/assest.js
@@ -0,0 +1,4 @@
+import sync_hz from '@/../public/sync_hz.svg';
+import sync_vz from '@/../public/sync_vz.svg';
+
+export { sync_hz, sync_vz };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,24 @@
-import Image from "next/image";
-import { Rubik_Doodle_Triangles } from "next/font/google";
+'use client';
+
+import Image from 'next/image';
+import { Rubik_Doodle_Triangles } from 'next/font/google';
+import { sync_hz } from './assest';
 
 const rubik_traingles = Rubik_Doodle_Triangles({
-  subsets: ["latin"],
-  weight: "400",
+  subsets: ['latin'],
+  weight: '400',
 });
 
 export default function Home() {
   return (
-    <main className=" bg-gray-900 h-screen p-2">
-      <div
-        className={`heading__container flex flex-col justify-center items-center text-white ${rubik_traingles.className} h-full`}
+    <main className=" h-screen bg-gray-900 p-2">
+      {/* <div
+        className={`heading__container flex flex-col items-center justify-center text-white ${rubik_traingles.className} h-full`}
       >
         <span className="text-[25rem]">Sync</span>
+      </div> */}
+      <div className="brand__container flex h-full items-center justify-center p-5">
+        <Image src={sync_hz} alt="brand logo" className="w-full" />
       </div>
     </main>
   );
