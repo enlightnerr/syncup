6644a2c538ef0e28fa103eea4ca99d32407f76c7
Prashant <prashantdubey0098@gmail.com>
2024-02-07 13:56:54 +0530

    added animations

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import { Rubik_Doodle_Triangles } from 'next/font/google';
-import { sync_hz } from './assest';
+import { sync_hz, sync_vz } from './assest';
 
 const rubik_traingles = Rubik_Doodle_Triangles({
   subsets: ['latin'],
@@ -18,7 +18,17 @@ export default function Home() {
         <span className="text-[25rem]">Sync</span>
       </div> */}
       <div className="brand__container flex h-full items-center justify-center p-5">
-        <Image src={sync_hz} alt="brand logo" className="w-full" />
+        <Image
+          src={sync_hz}
+          alt="brand logo"
+          className="hidden w-full animate-pulse md:block"
+        />
+        <Image
+          src={sync_vz}
+          alt="brand logo"
+          className="animate-pulse md:hidden"
+        />
+        {/* <div className="bg__container absolute z-10 h-1/2 w-full bg-cyan-200"></div> */}
       </div>
     </main>
   );
