7f8545ac88c06fc694070ef71525a484694cfb3a
Prashant <prashantdubey0098@gmail.com>
2024-02-24 01:16:07 +0530

    updated home page

--- src/app/(ui)/layout.jsx
+++ src/app/(ui)/layout.jsx
@@ -4,7 +4,7 @@ import '../globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Sync up your workspace | Sync',
+  title: 'Sync',
   description: '',
 };
 
